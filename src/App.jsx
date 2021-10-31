03868d0ad08b8403cd3bde1a33aa4020584dc1c2
gustavoccintrao <gcintrao2011@gmail.com>
2021-10-31 20:30:39 -0300

    Layout adjustments to the country container

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,72 @@
 // import { useState, useEffect, Fragment } from 'react';
 
-
 export default function App() {
-    const countrys = [
-        {
-            name: "brazil",
-            title: "Brasil",
-            img: 'img/brazil.png',
-            text: 'console.log("Olá, mundo!")'
-        },
-        {
-            name: "argentina",
-            title: "Argentina",
-            img: 'img/argentina.png',
-            text: 'console.log("¡Hola, mundo!")'
-        },
-        {
-            name: "usa",
-            title: "Estados Unidos",
-            img: 'img/usa.png',
-            text: 'console.log("Hello, world!")'
-        },
-    ]
+  const countrys = [
+    {
+      name: "brazil",
+      title: "Brasil",
+      img: "img/brazil.png",
+      text: (
+        <span>
+          <span>console</span>
+          <span className="log">{".log"}</span>
+          <span className="hello-world">('Olá, Mundo!')</span>
+        </span>
+      ),
+    },
+    {
+      name: "argentina",
+      title: "Argentina",
+      img: "img/argentina.png",
+      text: (
+        <span>
+          <span>console</span>
+          <span className="log">{".log"}</span>
+          <span className="hello-world">('Hola, Mundo!')</span>
+        </span>
+      ),
+    },
+    {
+      name: "usa",
+      title: "Estados Unidos",
+      img: "img/usa.png",
+      text: (
+        <span>
+          <span>console</span>
+          <span className="log">{".log"}</span>
+          <span className="hello-world">('Hello, World!')</span>
+        </span>
+      ),
+    },
+  ];
 
-    // function handleShowCountrys() {
-    //     console.log("Entrou na função")
-    //     countrys.map(country => {
-    //         return (
-    //             <div className="country-container">
-    //                 <img src={country.img} alt='' />
-    //                 <span>{country.title}</span>
-    //             </div>
-    //         )
-    //     })
-    // }
-
-    return (
-        <div>
-            <h3> Hello World Around The World </h3>
-            {countrys.map(country => {
-                return (
-                    <div className="country-container">
-                        <img src={country.img} alt='' />
-                        <p>{country.title}</p>
-                        <div className="hello-world-wrapper">
-                            <div class="window">
-                                <div class="titlebar">
-                                    <div class="buttons">
-                                        <div class="close">
-                                        </div>
-                                        <div class="minimize">
-                                        </div>
-                                        <div class="zoom">
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="content">
-                                    <p>{country.text}</p>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-                )
-            })}
-        </div>
-    )
-
-
-
+  return (
+    <div>
+      <h3> Hello World Around The World </h3>
+      {countrys.map((country) => {
+        return (
+          <div>
+            <div className="country-container">
+              <img src={country.img} alt="" />
+              <p>{country.title}</p>
+            </div>
+            <div className="hello-world-wrapper">
+              <div class="window">
+                <div class="titlebar">
+                  <div class="buttons">
+                    <div class="close"></div>
+                    <div class="minimize"></div>
+                    <div class="zoom"></div>
+                  </div>
+                </div>
+                <div class="content">
+                  <p>{country.text}</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
